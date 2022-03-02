7cf34a7292f155cd044be40dce8239e7eed96712
Taeyeon <mindc1891@gmail.com>
2022-03-02 20:12:56 +0900

    ReservationViewSet 작성

--- flight/urls.py
+++ flight/urls.py
@@ -3,8 +3,9 @@ from flight import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('flight',views.FlightViewSet)
+router.register('flight',views.FlightViewSet),
+router.register('reservation',views.ReservationViewSet),
 
 urlpatterns = [
-    path('flightService/', include(router.urls)),
+    path('flightService/',include(router.urls)),
 ]
--- flight/views.py
+++ flight/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
-from .serializers import FlightSerializer
-from .models import Flight
+from .serializers import FlightSerializer,ReservationSerializer
+from .models import Flight,Reservation
 
 # Create your views here.
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
-    serializer_class = FlightSerializer   
+    serializer_class = FlightSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer      
