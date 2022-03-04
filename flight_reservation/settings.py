b97fd74dacbe9026b8d035b8bf6b13e70ee468af
Taeyeon <mindc1891@gmail.com>
2022-03-04 10:36:53 +0900

    authentication 설정

--- flight_reservation/settings.py
+++ flight_reservation/settings.py
@@ -44,9 +44,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'passenger',
     'flight',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+    'rest_framework.authentication.TokenAuthentication',    
+    ]
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
